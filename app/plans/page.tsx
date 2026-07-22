b0bf86e7489a61056e3dbe57abab3441b452410f
Phillip Vu <pvu0452@gmail.com>
2026-07-23 00:46:58 +1000

    Improve payment page UI and payment methods

--- app/plans/page.tsx
+++ app/plans/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useEffect, useRef, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 /* -----------------------------
    TYPES
