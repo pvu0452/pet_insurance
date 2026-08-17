5cd184675bb32c6a2235efb4c45664f88011883e
Mitchell <mitch005thomas@gmail.com>
2026-08-17 22:19:42 +1000

    Added pages for success and cancel to handle payment outcomes. Will need to make the display of these pages more user-friendly and informative later.

---
+++ app/cancel/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+export default function PlanComparisonPage() {
+return (
+    <div className="min-h-screen">
+
+      <p>Payment failed</p>
+
+</div>
+
+  );
+}
